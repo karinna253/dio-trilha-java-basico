0f79e3ca315f0b1985b616efaf2a31e05930d776
karinna253 <karinasilva253@gmail.com>
2024-09-05 21:36:48 -0300

    Ajustes

--- bin/Operadores-Java/Operadores/OPeradores/src/App.java
+++ bin/Operadores-Java/Operadores/OPeradores/src/App.java
@@ -2,6 +2,4 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
-
-    String nomeCompleto = "LINGUAGEM" + "JAVA";
 }
