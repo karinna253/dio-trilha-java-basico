149ec83695e753852ae23de97d18d50376e13ce4
karinna253 <karinasilva253@gmail.com>
2024-09-18 21:38:00 -0300

    Meu projeto conta banco

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,9 +1,9 @@
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        int numero = Integer.valueOf(args[1]);
-        String agencia = args[2];
-        String nome_Cliente = args[3];
-        double saldo = Integer.valueOf(args[4]);
+        int numero = Integer.valueOf(args[0]);
+        String agencia = args[1];
+        String nome_Cliente = args[2];
+        double saldo = Integer.valueOf(args[3]);
 
         System.out.println ("Por favor, digite o número da Agência !");
         System.out.println(agencia);
@@ -11,3 +11,4 @@ public class contaTerminal {
         System.out.println("Ola "+ nome_Cliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta"+numero+"  e seu saldo de "+saldo+" já está disponível para saque.");
     }
 }
+
