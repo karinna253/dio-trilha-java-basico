b20528f09d5718a80094dd80fa8574a717d9a088
karinna253 <karinasilva253@gmail.com>
2024-09-30 04:49:44 -0300

    controle de fluxo

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,21 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+        }
+
+        ///Usando for/each
+        /*
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+        */
+
+    }
+    
+}
